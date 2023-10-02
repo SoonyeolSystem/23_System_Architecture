5334758268add01a2b368911c499b8a81af64c81
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2023-10-02 17:05:02 +0900

    Update talking_result_page.dart

--- frontend/soonyeol_architecture/lib/pages/talking/view/component/talking_result_page.dart
+++ frontend/soonyeol_architecture/lib/pages/talking/view/component/talking_result_page.dart
@@ -11,6 +11,7 @@ class TalkingResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Center(
         child: Container(
@@ -35,17 +36,137 @@ class TalkingResultPage extends StatelessWidget {
               SizedBox(height: 50),
               Container(
                 width: Common.getWidth - 100,
-                height: 350,
+                height: 300,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   color: Color(0xFF434343),
                 ),
-                alignment: Alignment.center,
+                alignment: Alignment.topLeft,
                 child: Material(
                   type: MaterialType.transparency,
-                  child: Text(
-                    '텍스트',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  child: Column(
+                    children:[
+                      Row(
+                        
+                children: [
+                  
+                  Padding(
+                    padding: EdgeInsets.only(left: 40.0, top: 30.0),
+                    child: Text(
+                      '스피킹 평균 속도',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  // ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 80.0, top: 30.0),
+                    child: Text(
+                      'asdf',
+                      style: TextStyle(fontSize: 24, color: Color(0xFF47CDD6)),
+                    ),
+                  // ),
+                ),
+               Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 30.0),
+                    child: Text(
+                      's',
+                      style: TextStyle(fontSize: 24, color: Colors.white)),
+                    ),
+                  // ),
+                
+                ],
+              ), 
+              Row(
+                        
+                children: [
+                  
+                  Padding(
+                    padding: EdgeInsets.only(left: 40.0, top: 30.0),
+                    child: Text(
+                      '대화 나눈 횟수',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  // ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 130.0, top: 30.0),
+                    child: Text(
+                      '3',
+                      style: TextStyle(fontSize: 24, color: Color(0xFF47CDD6)),
+                    ),
+                  // ),
+                ),
+               Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 30.0),
+                    child: Text(
+                      '회',
+                      style: TextStyle(fontSize: 24, color: Colors.white)),
+                    ),
+                  // ),
+                
+                ],
+              ),
+              Row(
+                        
+                children: [
+                  
+                  Padding(
+                    padding: EdgeInsets.only(left: 40.0, top: 30.0),
+                    child: Text(
+                      '대화 나눈 시간',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  // ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 110.0, top: 30.0),
+                    child: Text(
+                      '1.5',
+                      style: TextStyle(fontSize: 24, color: Color(0xFF47CDD6)),
+                    ),
+                  // ),
+                ),
+               Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 30.0),
+                    child: Text(
+                      'm',
+                      style: TextStyle(fontSize: 24, color: Colors.white)),
+                    ),
+                  // ),
+                
+                ],
+              ),
+              Row(
+                        
+                children: [
+                  
+                  Padding(
+                    padding: EdgeInsets.only(left: 40.0, top: 30.0),
+                    child: Text(
+                      '토킹 점수',
+                      style: TextStyle(fontSize: 24, color: Color(0xFF47CDD6)),
+                    ),
+                  // ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 180.0, top: 30.0),
+                    child: Text(
+                      '7',
+                      style: TextStyle(fontSize: 24, color: Color(0xFF47CDD6)),
+                    ),
+                  // ),
+                ),
+               Padding(
+                    padding: EdgeInsets.only(left: 10.0, top: 30.0),
+                    child: Text(
+                      '점',
+                      style: TextStyle(fontSize: 24, color: Colors.white)),
+                    ),
+                  // ),
+                
+                ],
+              ),
+                                 ]
                   ),
                 ),
               ),
@@ -62,6 +183,29 @@ class TalkingResultPage extends StatelessWidget {
                   ),
                 ),
               ),
+              SizedBox(height: 40),
+                Container(
+                  width: Common.getWidth - 100,
+                  height: 200,
+                  // alignment: Alignment.center,
+                  child: SingleChildScrollView(
+                    
+                    // physics: const BouncingScrollPhysics(),
+                    // controller: controller.scrollcontroller.value,
+                    child: Text(
+                      '이곳에 긴 글을 넣어주세요. 이 글이 화면보다 길어지면 스크롤이 가능하도록 설정되었습니다. '
+                      'Flutter는 이러한 기능을 지원하기 위해 SingleChildScrollView와 같은 다양한 위젯을 제공합니다. '
+                      '이를 통해 사용자는 긴 컨텐츠를 편리하게 탐색할 수 있습니다. '
+                      '글이 계속 이어지면, 이 글은 화면의 크기를 초과하여 아래로 확장됩니다. '
+                      '따라서 사용자는 화면을 스크롤하여 글의 나머지 부분을 볼 수 있습니다. '
+                      '이와 같이 Flutter는 사용자 경험을 향상시키기 위한 다양한 위젯과 도구를 제공합니다.',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                      
+                    ),
+                  ),
+                ),
+
+              SizedBox(height: 50),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: ElevatedButton(
