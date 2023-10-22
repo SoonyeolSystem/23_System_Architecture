1f306e18a6ef3b9720ff3b347d4e9c4197f565cf
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-10-22 20:06:33 +0900

    fix error

--- frontend/soonyeol_architecture/lib/pages/talking/controller/talking_view_controller.dart
+++ frontend/soonyeol_architecture/lib/pages/talking/controller/talking_view_controller.dart
@@ -1,25 +1,24 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:soonyeol_architecture/common/common.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
+
 import '../../../restAPI/models/Talking.dart';
 
 class TalkingViewController extends GetxController {
   static TalkingViewController get instance => Get.find<TalkingViewController>();
 
-  late final WebSocketChannel? channel;  
+  late final WebSocketChannel? channel;
   var isListening = false.obs;
   var speechText = 'press the Mic button  and start speaking'.obs;
   late SpeechToText speechToText;
 
   FlutterTts tts = FlutterTts();
   final TextEditingController con = TextEditingController();
-  late final WebSocketChannel? channel;
 
   @override
   void onInit() async {
@@ -40,26 +39,24 @@ class TalkingViewController extends GetxController {
   }
 
   void listen() async {
-    if(!isListening.value){
-      bool available=await speechToText.initialize(
-        onStatus: (val){},
-        onError: (val){},
+    if (!isListening.value) {
+      bool available = await speechToText.initialize(
+        onStatus: (val) {},
+        onError: (val) {},
       );
-      if(available){
+      if (available) {
         isListening.value = true;
-        speechToText.listen(
-          onResult: (val) {
-            speechText.value = val.recognizedWords;
-          }
-        );
+        speechToText.listen(onResult: (val) {
+          speechText.value = val.recognizedWords;
+        });
       }
-    }
-    else{
+    } else {
       isListening.value = false;
       speechToText.stop();
       speechText.value = '';
     }
   }
+
   void sendMesage() {
     channel?.sink.add('Hello');
   }
@@ -75,46 +72,22 @@ class TalkingViewController extends GetxController {
   Future<void> getTalkingList() async {
     List<Map<String, dynamic>> fakeJsonData = [
       {"character": "me", "script": "Oh my god! Where are we?"},
-      {
-        "character": "you",
-        "script": "I have no idea, but we need to find water and shelter fast!"
-      },
+      {"character": "you", "script": "I have no idea, but we need to find water and shelter fast!"},
       {"character": "me", "script": "But I can't see anything but sand here!"},
-      {
-        "character": "you",
-        "script":
-            "Let's start walking in that direction. Maybe we'll find something."
-      },
-      {
-        "character": "me",
-        "script":
-            "Oh my god! Where are we?Oh my god! Where are we?Oh my god! Where are we?Oh my god! Where are we?"
-      },
-      {
-        "character": "you",
-        "script": "I have no idea, but we need to find water and shelter fast!"
-      },
+      {"character": "you", "script": "Let's start walking in that direction. Maybe we'll find something."},
+      {"character": "me", "script": "Oh my god! Where are we?Oh my god! Where are we?Oh my god! Where are we?Oh my god! Where are we?"},
+      {"character": "you", "script": "I have no idea, but we need to find water and shelter fast!"},
       {"character": "me", "script": "Oh my god! Where are we?"},
-      {
-        "character": "you",
-        "script": "I have no idea, but we need to find water and shelter fast!"
-      },
+      {"character": "you", "script": "I have no idea, but we need to find water and shelter fast!"},
       {"character": "me", "script": "Oh my god! Where are we?"},
       {"character": "you", "script": "I have no idea"},
-      {
-        "character": "me",
-        "script": "Oh my god! Where are we? gggggggg okay let's take a look."
-      },
+      {"character": "me", "script": "Oh my god! Where are we? gggggggg okay let's take a look."},
       {"character": "you", "script": "hello!"},
       {"character": "me", "script": "Oh my god! Where are we?"},
-      {
-        "character": "you",
-        "script": "I have no idea, but we need to find water and shelter fast!"
-      },
+      {"character": "you", "script": "I have no idea, but we need to find water and shelter fast!"},
     ];
 
-    List<Talking> fakeTalkings =
-        fakeJsonData.map((jsonData) => Talking.fromJson(jsonData)).toList();
+    List<Talking> fakeTalkings = fakeJsonData.map((jsonData) => Talking.fromJson(jsonData)).toList();
     talkingList.value = fakeTalkings;
     talkingList.refresh();
   }
