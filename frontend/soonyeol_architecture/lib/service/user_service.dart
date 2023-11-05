2ff5031e2c22dc42d50531b3091a2e960f5bed06
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2023-11-05 23:40:13 +0900

    Add login function in. user_service

--- frontend/soonyeol_architecture/lib/service/user_service.dart
+++ frontend/soonyeol_architecture/lib/service/user_service.dart
@@ -1,13 +1,18 @@
 import 'package:get/get.dart';
 import 'package:soonyeol_architecture/common/common.dart';
+import 'package:soonyeol_architecture/common/service_response.dart';
+import 'package:soonyeol_architecture/pages/main/view/main_view_page.dart';
+import 'package:soonyeol_architecture/restAPI/api_service.dart';
+import 'package:soonyeol_architecture/restAPI/response/login_response.dart';
 
 class UserService extends GetxService {
   static UserService get instance => Get.find<UserService>();
 
   bool logining = false;
-  bool isLogin = false;
+  // bool isLogin = false;
   String userId = '';
   String nickname = '';
+  bool isLogin=false;
   Future<UserService> init() async {
     Common.logger.d('$runtimeType init!');
 
@@ -15,5 +20,34 @@ class UserService extends GetxService {
     Common.logger.d('Nickname: ${nickname}');
     return this;
   }
-  
+
+  Future<bool> login(String loginId, String password) async {
+    ApiResponse<LoginResponse> response =
+                          await ApiService.instance.login(loginId, password);
+
+                      if (response.statusCode == 200) {
+                        // 로그인 성공
+                        Get.offAllNamed(MainViewPage.url);
+
+                        Get.snackbar("success", "로그인에 성공하였습니다.");
+                        userId=response.userId!;
+                        nickname = response.nickname!;
+                        Get.snackbar('User ID: ${userId}', 'Nickname: ${nickname}');
+
+                        Common.logger.d('User ID: ${userId}');
+Common.logger.d('Nickname: ${nickname}');
+                      } else if (response.statusCode == 400) {
+                        // ID가 존재하지 않음
+                        Get.snackbar(
+                            "ID/Password not Found", "ID/비밀번호가 존재하지 않습니다.");
+                      } else {
+                        // 기타 오류   
+                        Get.snackbar(
+                            "Error", response.errorMsg ?? "알 수 없는 오류가 발생했습니다.");
+                      }
+                          return false;
+  }
+  //  Future<bool> isLogin() async {
+
+  //  }
 }
