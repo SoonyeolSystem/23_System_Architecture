a08c39bf4637a41a5c2f074efbf34d9b58773fa2
2sojeong <dlgur200123@naver.com>
2023-11-15 18:57:23 +0900

    const 지움

--- frontend/soonyeol_architecture/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/soonyeol_architecture/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,12 @@ import Foundation
 
 import flutter_secure_storage_macos
 import flutter_tts
-import path_provider_foundation
 import speech_to_text_macos
 import text_to_speech_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   TextToSpeechMacOsPlugin.register(with: registry.registrar(forPlugin: "TextToSpeechMacOsPlugin"))
 }
