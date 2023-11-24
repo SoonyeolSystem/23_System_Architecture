7f0f745117566690acecf79ee889262bcc9ad96c
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-11-24 15:31:55 +0900

    update url

--- frontend/soonyeol_architecture/lib/common/common.dart
+++ frontend/soonyeol_architecture/lib/common/common.dart
@@ -5,10 +5,10 @@ class Common extends GetxService {
   static Common get instance => Get.find<Common>();
   static const bool isDev = true;
   static Logger logger = Logger(filter: MyFilter());
-  // static const String userServiceUrl = "http://43.200.93.144/user/";
-  // static const String communityServiceUrl = "http://43.200.93.144/community/";
-  static const String userServiceUrl = "http://127.0.0.1:8081/";
-  static const String communityServiceUrl = "http://127.0.0.1:8080/";
+  static const String userServiceUrl = "http://43.201.70.13:7001/user/";
+  static const String communityServiceUrl = "http://43.201.70.13:7001/community/";
+  // static const String userServiceUrl = "http://127.0.0.1:8081/";
+  // static const String communityServiceUrl = "http://127.0.0.1:8080/";
   static const String websocketUrl = "wss://h78bsknjuf.execute-api.ap-northeast-2.amazonaws.com/dev";
   // static const String userServiceUrl="https://kq7a0ulv2i.execute-api.ap-northeast-2.amazonaws.com/";
   static double get getWidth => GetPlatform.isMobile ? Get.width : 500;
